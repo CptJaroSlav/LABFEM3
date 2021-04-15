40ab2eed27dc9d461edd330a07aa4b96df3b24ac
CptJaroSlav <jaroslav.bialic@wp.pl>
2021-04-15 14:50:47 +0200

    Zmiana 1.3

--- LAB3.py
+++ LAB3.py
@@ -2,3 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def [parametry_sterujace] = inicjalizacja_parametrow_sterujacych(PRZEDZIAL, WAR_BRZEGOWE, RODZ_WAR_BRZEG)
+
+def [definicja_geometrii] = definicja_parametrow_geo_i_fiz(PRZEDZIAL, G_WEZLY, c,f)
