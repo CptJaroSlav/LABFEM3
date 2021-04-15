b83a8c5af79c219e81f148b637dbc125f224eeba
CptJaroSlav <jaroslav.bialic@wp.pl>
2021-04-15 15:16:35 +0200

    Zmiana 1.6

--- LAB3.py
+++ LAB3.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+c = 0
+f = 0
 
 wezly = np.array([[1, 0],
                   [2, 1],
@@ -17,5 +19,4 @@ twb_R = 'D'
 wwb_L = 0
 wwb_R = 1
 
-
-
+#def genTABLICEGEO(x_a, x_b, x):
