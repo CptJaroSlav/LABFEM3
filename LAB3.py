504c88acd95ca904b50a2dfd23ebbc8a64b3af4f
CptJaroSlav <jaroslav.bialic@wp.pl>
2021-04-15 15:31:39 +0200

    Zmiana 1.7

--- LAB3.py
+++ LAB3.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 c = 0
 f = 0
+n = 100
 
 wezly = np.array([[1, 0],
                   [2, 1],
@@ -19,4 +20,19 @@ twb_R = 'D'
 wwb_L = 0
 wwb_R = 1
 
-#def genTABLICEGEO(x_a, x_b, x):
+def genTABLICEGEO(x_a, x_b, x):
+
+    temp = (x_b-x_a)/(x-1)
+    array = np.array([1,0])
+
+    for i in range(1, x, 1):
+        array = np.block([
+            [array],
+            [i+1, i * temp + x_a],
+        ])
+    return array
+
+
+print(genTABLICEGEO(1,4,4))
+
+
