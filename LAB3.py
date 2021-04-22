552f50eef8d63c7bc008c66e4c40a4c0c4aef6ef
CptJaroSlav <jaroslav.bialic@wp.pl>
2021-04-22 15:41:17 +0200

    Zmiana2204v2

--- LAB3.py
+++ LAB3.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.integrate as spint
 c = 0
 f = lambda x: 0*x #wymuszenie
 
-x_a = 4
-x_b = 10
-x = 7 #ilosc wezlow NODES
+x_a = 0
+x_b = 1
+x = 5 #ilosc wezlow NODES
 
 twb_L = 'D'
 twb_R = 'D'
@@ -15,7 +15,7 @@ wwb_L = 0
 wwb_R = 1
 
 WB = [{"ind":1, "typ":'D', "wartosc":1},
-      {"ind":2, "typ":'D', "wartosc":2}]
+      {"ind":x, "typ":'D', "wartosc":2}]
 
 def genTABLICEGEO(x_a, x_b, x):
 
@@ -72,7 +72,71 @@ print(A)
 print(b)
 
 
-#def Bazowa(x):
+def FunkcjeBazowe(x):
+    #x stpoień funkcji kształtu
+    #zwraca liste funkcji kształtu
+
+    if x==0:
+        f = (lambda x: 1+0*x)
+        df = (lambda x: 0*x)
+
+    elif x==1:
+
+        f = (lambda x: -1/2*x + 1/2, lambda x: 0.5*x + 0.5)
+        df = (lambda x: -1/2 + 0 * x, lambda x: 0.5 + 0 * x )
+
+    #elif n==2:
+
+        #f = (lambda,lambda,lambda)
+    else:
+        raise Exception("Błąd")
+
+    return f,df
+stopien_funkcji_bazowych = 1
+phi,dphi = FunkcjeBazowe(stopien_funkcji_bazowych)
+
+print(phi)
+print(dphi)
+
+xx = np.linspace(-1,1,101)
+plt.plot(xx,phi[0](xx),'r')
+plt.plot(xx,phi[1](xx),'g')
+plt.plot(xx,dphi[0](xx),'b')
+plt.plot(xx,dphi[1](xx),'c')
+plt.show()
+
+#Preprocesing
+
+liczbaElementow = np.shape(elementy)[0]
+for ee in np.arange(0,liczbaElementow):
+
+    elemRowInd = ee
+    elemGlobalInd = elemety[ee,0]
+
+    elemWezel1 = elemety[ee,1]  #indeks wezla poczatkowego elementu ee
+    elemWezel2 = elemety[ee, 2] # indeks wezla koncowego elementu ee
+
+    Ml = np.zeros(stopien_funkcji_bazowych + 1, stopien_funkcji_bazowych + 1)
+
+    def Aij(df_i, df_j, c, f_i, f_j):
+
+        fun_podc = lambda x: -df_i(x)*df_j(x) + c * f_i(x)*f_j(x)
+
+        return fun_podc
+
+    p_a = wezly[elemWezel1-1,1]
+    p_b = wezly[elemWezel2-1,1]
+
+    J = (p_b-p_a)/2
+
+    Ml[0,0] = J * spint.quad(Aij(dphi[0],dphi[0],c,phi[0],phi[0]),-1,1)
+
+
+
+
+
+
+
 
 
 
